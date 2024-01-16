241ccbac5424c08004a2f7ab3eae1bb07e455708
SopGui <sophie.guile@softwire.co.uk>
2024-01-16 19:14:50 +0000

    read todos from file

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import os;
 
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
+def read_todos(filename):
+    print(os.getcwd())
+    with open(filename, 'r') as todo_file:
+        todo_items = todo_file.read()
+    return todo_items.split('\n')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+   todos = read_todos('todo_app/todos.txt')
+   return render_template('index.html', todos=todos)
