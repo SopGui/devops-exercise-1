25ab66e42a3301159b480f08e7888901d89242aa
SopGui <sophie.guile@softwire.co.uk>
2024-01-17 17:46:57 +0000

    add deleting items, minor style improvements

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, redirect
-from todo_app.todoItems.todoItemManager import get_todos, add_todo
+from todo_app.todoItems.todoItemManager import todo_list
 
 from todo_app.flask_config import Config
 
@@ -8,13 +8,21 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-   todos = get_todos()
+   todos = todo_list.get_todos()
    return render_template('index.html', todos=todos)
 
 @app.route('/add-item', methods=['POST'])
 def add_item():
     new_item = request.form.get('new-item')
-    add_todo(new_item)
+    todo_list.add_todo(new_item)
     return redirect('/', code=302)
     
-    
+@app.route('/delete-item/<item_id>', methods=['POST'])
+def delete_item(item_id):
+    todo_list.delete_todo(item_id)
+    return redirect('/', code=302)
+
+@app.route('/toggle-check-item/<item_id>', methods=['POST'])
+def toggle_check_item(item_id):
+    print(item_id)
+    return redirect('/', code=302)
--- todo_app/todoItems/todoItemManager.py
+++ todo_app/todoItems/todoItemManager.py
@@ -1,16 +1,39 @@
-FILENAME = "todo_app/todoItems/todos.json"
 import json
 import uuid
 
-def get_todos():
-    with open(FILENAME, 'r') as todo_file:
-        todo_items = todo_file.read()
-    return json.loads(todo_items)
+class TodoList:
+    def __init__(self, filename):
+        self.filename = filename
+        self.todo_list = self.read_todos_from_file()
 
-def add_todo(new_item):
-    entry = {'id': str(uuid.uuid4()), 'title': new_item}
-    todos = get_todos()
-    todos.append(entry)
+    def read_todos_from_file(self):
+        with open(self.filename, 'r') as todo_file:
+            todos_from_file = todo_file.read()
+        return json.loads(todos_from_file)
+    
+    def save_todos_to_file(self):
+        with open(self.filename, 'w') as todo_file:
+            json.dump(self.todo_list, todo_file)
 
-    with open(FILENAME, 'w') as todo_file:
-        json.dump(todos, todo_file)
+    def get_todos(self):
+        return self.todo_list
+    
+    def add_todo(self, new_item_title):
+        new_todo = {'id': str(uuid.uuid4()), 'title': new_item_title }
+        self.todo_list.append(new_todo)
+        self.save_todos_to_file()
+
+    def delete_todo(self, id_to_delete):
+        todo_list_without_id_to_delete = [todo_item for todo_item in self.todo_list if todo_item["id"] != id_to_delete]
+
+        if not len(todo_list_without_id_to_delete) < len(self.todo_list):
+            return False
+
+        self.todo_list = todo_list_without_id_to_delete
+        self.save_todos_to_file()
+
+todo_list = TodoList("todo_app/todoItems/todos.json")
+    
+
+    
+         
