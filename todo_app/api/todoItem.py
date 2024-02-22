226841409fff2938f1870153526f0cc346f30fef
SopGui <sophie.guile@softwire.co.uk>
2024-02-22 18:11:27 +0000

    finish api code, remove old code, make todo item class

---
+++ todo_app/api/todoItem.py
@@ -0,0 +1,45 @@
+from enum import Enum
+
+class TodoItemStatus(Enum):
+    NOT_STARTED = 1
+    IN_PROGRESS = 2
+    COMPLETE = 3
+
+not_started_text = "Not Started"
+in_progress_text = "In Progress"
+complete_text = "Complete"
+
+def get_string_as_todo_item_status(status_string: str):
+    if status_string == not_started_text:
+        return TodoItemStatus.NOT_STARTED
+    if status_string == in_progress_text:
+        return TodoItemStatus.IN_PROGRESS
+    if status_string == complete_text:
+        return TodoItemStatus.COMPLETE
+    return None
+
+def get_todo_item_status_as_string(status: TodoItemStatus):
+    if status == TodoItemStatus.NOT_STARTED:
+        return not_started_text
+    if status == TodoItemStatus.IN_PROGRESS:
+        return in_progress_text
+    if status == TodoItemStatus.COMPLETE:
+        return complete_text
+    return "Unkown"
+
+class TodoItem():
+    def __init__(self, id: str, title: str, status):
+        self.id = id
+        self.title = title
+
+        if type(status) == TodoItemStatus:
+            self.status = status
+        else:
+            self.status = get_string_as_todo_item_status(status)
+
+    def get_status_as_string(self):
+        return get_todo_item_status_as_string(self.status)
+    
+    def __str__(self):
+       return f"Card - ID: {self.id} Title: {self.title}, Status: {self.status}"
+        
--- todo_app/api/trelloApi.py
+++ todo_app/api/trelloApi.py
@@ -1,56 +1,43 @@
 import requests
 import os
-import todo_app.api.todoItemStatus as todoItemStatus
-#import todoItemStatus
+import todo_app.api.todoItem as todoItem
 
 base_url = "https://api.trello.com/1"
 trello_board_id = "65d6337573a6275aed0ec68a"
-trello_list_id="65d6362e050962e28cf00ae5"
 
 not_started_list_id="65d733960723c15693565524"
 in_progress_list_id="65d7339b144d16a2cd686632"
 complete_list_id="65d733a812de2dee30a1c327"
 
 def get_list_id_from_status(status):
-    if status == todoItemStatus.Todo_item_status.NOT_STARTED:
+    if status == todoItem.TodoItemStatus.NOT_STARTED:
         return not_started_list_id
-    elif status == todoItemStatus.Todo_item_status.IN_PROGRESS:
+    if status == todoItem.TodoItemStatus.IN_PROGRESS:
         return in_progress_list_id
-    elif status == todoItemStatus.Todo_item_status.COMPLETE:
+    if status == todoItem.TodoItemStatus.COMPLETE:
         return complete_list_id
-    else:
-        return "Unknown"
+    return "Unknown"
 
 def get_status_from_list_id(list_id):
     if list_id == not_started_list_id:
-        return todoItemStatus.Todo_item_status.NOT_STARTED
-    elif list_id == in_progress_list_id:
-        return todoItemStatus.Todo_item_status.IN_PROGRESS
-    elif list_id == complete_list_id:
-        return todoItemStatus.Todo_item_status.COMPLETE
-    else:
-        return None
+        return todoItem.TodoItemStatus.NOT_STARTED
+    if list_id == in_progress_list_id:
+        return todoItem.TodoItemStatus.IN_PROGRESS
+    if list_id == complete_list_id:
+        return todoItem.TodoItemStatus.COMPLETE
+    return None
 
-# TODO : switch over to using environment varialbes before submitting
 def get_api_key():
-    #return "227ed8615e0f8b4a002244483db5ec7a"
     return os.getenv('TRELLO_API_KEY')
 
 def get_api_token():
-    #return "ATTA33185227669ef97599b66df3458fcd057fbc93771efa15e301c0f61c151d015e56F2752A"
     return os.getenv('TRELLO_API_TOKEN')
 
 def display_error(response):
     print(f"ERROR: CODE {response.status_code} - {response.text}")
 
 def map_card(card_response_json):
-    # TODO: should be a todo item class instance, rather than a dict
-    return {
-        "id": card_response_json["id"],
-        "title": card_response_json["name"],
-        # TODO: status is currently in description - I want to allow cards to have descriptions, so it'd be good to find another way of storing status (maybe by having separate lists)
-        "status": get_status_from_list_id(card_response_json["idList"]),
-    }
+    return todoItem.TodoItem(card_response_json["id"], card_response_json["name"], get_status_from_list_id(card_response_json["idList"]))
 
 def get_cards():
     url = f"{base_url}/boards/{trello_board_id}/cards?key={get_api_key()}&token={get_api_token()}"
@@ -59,13 +46,13 @@ def get_cards():
     
     if response.status_code >= 400:
         display_error(response)
-        return None
+        return response.status_code
 
     cards = list(map(map_card, response.json()))
-    cards = list(filter(lambda card : card["status"] != None, cards))
+    cards = list(filter(lambda card : card.status != None, cards))
     return cards
 
-def create_card(title, status : todoItemStatus.Todo_item_status):
+def create_card(title, status : todoItem.TodoItemStatus):
     list_id = get_list_id_from_status(status)
     url = f"{base_url}/cards?idList={list_id}&key={get_api_key()}&token={get_api_token()}"
 
@@ -79,17 +66,38 @@ def create_card(title, status : todoItemStatus.Todo_item_status):
         display_error(response)
         return None
     
-    return map_card(response.json())
+    return response.status_code
 
 def delete_card(card_id):
+    print()
     url=f"{base_url}/cards/{card_id}?key={get_api_key()}&token={get_api_token()}"
 
     response = requests.request("DELETE", url)
+
+    print(response.status_code)    
+    print(response.text)
 
     if response.status_code >= 400:
         display_error(response)
 
     return response.status_code
 
-print(create_card("done", todoItemStatus.Todo_item_status.COMPLETE))
-#print(get_cards())
+def set_card_status(card_id, new_status: todoItem.TodoItemStatus):
+    url=f"{base_url}/cards/{card_id}?key={get_api_key()}&token={get_api_token()}"
+
+    body = {
+        "idList": get_list_id_from_status(new_status),
+    }
+
+    print(new_status)
+    print(body)
+
+    response = requests.request("PUT", url, data=body)
+
+    print(response.text)
+
+    if response.status_code >= 400:
+        display_error(response)
+        return response.status_code
+
+    return response.status_code
